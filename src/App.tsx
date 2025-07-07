1fed6871e6e1371235834b06c545798065cd9df3
Kodandarao Chellapilli <briggs2019@go.byuh.edu>
2025-07-06 23:20:03 -0600

    Setup basic TicTacToe component and App structure, removed unused logo import

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import { TicTacToe } from "./components/tic-tac-toe/Tic-tac-toe";
+
+function App() {
+  return (
+    <div className="App">
+      <TicTacToe />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/tic-tac-toe/Tic-tac-toe.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const TicTacToe = () => {
+  return (
+    <div className="game">
+      <h1>Tic Tac Toe</h1>
+      Board
+    </div>
+  );
+};
